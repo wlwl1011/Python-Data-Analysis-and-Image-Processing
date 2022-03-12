7da39bf882ad80c1840333aec471c4bde12dd6de
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2022-03-12 21:08:58 +0900

    image transformation

--- openCV/7.py
+++ openCV/7.py
@@ -1,15 +1,14 @@
-#이미지 회전
-
+#이미지 위치 변경
 import cv2
 import numpy as np
 
 image = cv2.imread('/Users/gimminji/Desktop/2022/Python-Data-Analysis-and-Image-Processing/openCV/cat.jpg')
 
 height, width = image.shape[:2]
-print(image[:2])
-
-M = cv2.getRotationMatrix2D((width/2, height/2), 90, 0.5)
+#x= 열 이동 y = 행이동
+M = np.array([[1,0,50],[0,1,10]],dtype=np.float32)
 dst = cv2.warpAffine(image,M,(width,height))
 
-#cv2.imshow('Hi',dst)
-#cv2.waitKey(0)
+cv2.imshow('image',dst)
+cv2.waitKey(0)
+
--- openCV/8.py
+++ openCV/8.py
@@ -6,7 +6,8 @@ import numpy as np
 image = cv2.imread('/Users/gimminji/Desktop/2022/Python-Data-Analysis-and-Image-Processing/openCV/cat.jpg')
 
 height, width = image.shape[:2]
-print(image[:2])
+print(image.shape)
+print(image.shape[:2])
 
 M = cv2.getRotationMatrix2D((width/2, height/2), 90, 0.5)
 dst = cv2.warpAffine(image,M,(width,height))
