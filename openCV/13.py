f83a272ab92b12b08abcc375fdc72472a4bbed92
wlwl1011 <62577565+wlwl1011@users.noreply.github.com>
2022-03-13 10:59:03 +0900

    create rectangle on image

---
+++ openCV/13.py
@@ -0,0 +1,11 @@
+#사각형 그리기
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+image = np.full((512,512,3),255,np.uint8)
+image = cv2.rectangle(image,(20,20),(255,255),(255,0,0),10)
+
+plt.imshow(image)
+plt.show()
